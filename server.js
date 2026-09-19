aea0e35f0a77f9cb52dc8fae2f9e1d83e19786ae
kenkenny11 <95muragekelvin@gmail.com>
2026-09-19 09:45:48 +0300

    Finish affiliate query fix

--- server.js
+++ server.js
@@ -2461,7 +2461,7 @@ async function addAffiliateTrackingToPost(post, content) {
   const { rows } = await pool.query(
     `SELECT *
      FROM affiliate
-     WHERE active = 1       AND a.affiliate_url IS NOT NULL       AND a.affiliate_url <> ''
+     WHERE active = 1       AND affiliate_url IS NOT NULL       AND affiliate_url <> ''
      ORDER BY id ASC`
   );
 
